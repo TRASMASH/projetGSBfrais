da0b1588ce7846728f0bec181f55b606af4f233b
Younes LOUREIRO <ylour@lmdsio.com>
2025-12-05 09:43:50 +0100

    création logoutAPI

--- app/Http/Controllers/VisiteurController.php
+++ app/Http/Controllers/VisiteurController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
-
+use App\Models\Visiteur;
+use Exception;
 use Illuminate\Http\Request;
 use App\Services\VisiteurService;
 class VisiteurController extends Controller{
@@ -28,5 +30,63 @@ class VisiteurController extends Controller{
         $service->signOut();
         return redirect(url('/'));
     }
+    public function initPasswordAPI(Request $request)
+    {
+        try {
+            $request->validate(['pwd_visiteur' => 'required|min:3']);
+            $hash = bcrypt($request->json('pwd_visiteur'));
+            Visiteur::query()->update(['pwd_visiteur' => $hash]);
+            return response()->json(['status' => 'mots de passe réinitialisés']);
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
+    }
+
+        public function authAPI(Request $request)
+    {
+        try {
+            $request->validate([
+                'login' => 'required',
+                'pwd' => 'required'
+            ]);
+            $login = $request->json("login");
+            $pwd = $request->json("pwd");
+            $identifiants = ['login_visiteur' => $login, 'password' => $pwd];
+            if (!Auth::attempt($identifiants)) {
+                return response()->json(['error' => 'Identifiants incorrects'], 401);
+            }
+
+            $visiteur = $request->user();
+            $token = $visiteur->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'token' => $token,
+                'token_type' => 'Bearer',
+                'visiteur' => [
+                    'id_visiteur' => $visiteur->id_visiteur,
+                    'nom_visiteur' => $visiteur->nom_visiteur,
+                    'prenom_visiteur' => $visiteur->prenom_visiteur,
+                    'type_visiteur' => $visiteur->type_visiteur,
+                ]
+            ]);
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
+    }
+    public function logoutAPI(Request $request)
+    {
+        try {
+            $request->user()->tokens()->delete();
+            return response()->json(['status' => 'utilisateur déconnecté']);
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
+    }
+
+    public function unauthorizedAPI()
+    {
+        return response()->json(['error' => 'accès non autorisé'], 401);
+    }
+
+
 
 }
